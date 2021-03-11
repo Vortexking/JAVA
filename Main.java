fbad1968935d25a9443e640df443cc59ba88f69b
Vortexking <80475794+Vortexking@users.noreply.github.com>
2021-03-11 23:02:34 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+
+public class Main
+{   
+    public  int fibo(int a)
+    {   int c;
+        int i;
+        int var1=0;
+        int var2=1;
+      for(i=2;i<a;i++)
+      {
+       c = var1+var2;
+       var1=var2;
+       var2=c;
+      }
+      
+      return var2;
+      
+    }
+    
+    public static void main(String[]args)
+    { 
+      System.out.println("ENTER THE NUMBER");
+      Scanner sc = new Scanner(System.in);
+      int a = sc.nextInt();
+     
+      Main rf = new Main(); 
+      int output = rf.fibo(a);
+      System.out.println(output);
+      sc.close();
+    
+    }
+
+}
